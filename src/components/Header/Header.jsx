0b7ecc6d542eb47fd9002851fb7e39989929c012
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-12 00:55:02 -0300

    feat/ add searchBar component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Header() {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header" style={{ background: "red" }}>
+      <div className="container" style={{ maxWidth: "800px" }}>
+        <form>
+          <input
+            type="search"
+            placeholder="Buscar produtos"
+            className="search__input"
+            required
+          />
+
+          <button type="submit" className="search__button"></button>
+        </form>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
