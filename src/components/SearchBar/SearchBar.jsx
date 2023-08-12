a4a3e4941e07f5ac00e1aa88cb3f8a42fda33e7a
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-12 01:55:15 -0300

    feat/ add useState

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <form className="search-bar">
       <input
         type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
+        onChange={({ target }) => setSearchValue(target.value)}
         required
       />
 
