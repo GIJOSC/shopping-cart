e137e116afac31578f74a7436c195205160b2d96
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-12 21:31:45 -0300

    feat/ add useEffect to Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from "react";
 
 import "./Products.css";
+import fetchProducts from "../../api/fetchProducts";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts("notebook").then(() => {});
+
+    setProducts(response);
+  }, []);
+
+  console.log(products);
+
   return <section className="products container">products</section>;
 }
 
