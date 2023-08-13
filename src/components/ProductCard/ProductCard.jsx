05e4d024d650a31e826bf0fe500cd42417321dd0
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-12 23:13:37 -0300

    feat/ add card product structure

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-function ProductCard()  {
-  return ( 
+function ProductCard() {
+  return (
     <section className="product-card">
-
       <img src="" alt="product" className="card__Image" />
 
+      <div className="card__infos">
+        <h2 className="card__price">R$ 2.000</h2>
+        <h2 className="card__title">Notebook</h2>
+      </div>
+      <button type="button" className="button__add-cart">
+        +
+      </button>
     </section>
   );
 }
- 
+
 export default ProductCard;
