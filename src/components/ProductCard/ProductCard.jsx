4e106ff60782d415da97b7b0bb6a4936bc532788
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-13 10:13:01 -0300

    fix/ add regex to image and fix resolution

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -6,14 +6,20 @@ import "./ProductCard.css";
 
 function ProductCard({ data }) {
   const { title, thumbnail, price } = data;
+
   return (
     <section className="product-card">
-      <img src={thumbnail} alt="product" className="card__Image" />
+      <img
+        src={thumbnail.replace(/\w.jpg/gi, "W.jpg")}
+        alt="product"
+        className="card__Image"
+      />
 
       <div className="card__infos">
         <h2 className="card__price">{price}</h2>
         <h2 className="card__title">{title}</h2>
       </div>
+
       <button type="button" className="button__add-cart">
         <LiaCartPlusSolid />
       </button>
