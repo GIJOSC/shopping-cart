cd5345852c5ddd846671136b9f1d7cbe30c23ba3
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-15 07:11:19 -0300

    feat/ add cart component structure

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <section>
+      <div>items do carrinho</div>
+      <div>resumo do carrinho</div>
+    </section>
+  );
+}
+
+export default Cart;
