245314ba85c6437f670b86dc896146e4d4ffac9d
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-15 06:46:15 -0300

    feat/remove useState from products and move to provider

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -4,10 +4,13 @@ import PropTypes from "prop-types";
 
 function Provider({ children }) {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const value = {
     products,
     setProducts,
+    loading, 
+    setLoading
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
