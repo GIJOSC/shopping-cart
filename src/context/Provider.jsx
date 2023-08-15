23b00c28320e4fb7e8f747fde15dd7377c7a02fa
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-14 23:20:56 -0300

    feat/ add context structure

---
+++ src/context/Provider.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import propTypes from "prop-types";
+import AppContext from "./AppContext";
+
+function Provider({ children }) {
+  const [name, setName] = useState("Testando ...");
+
+  const value = {
+    name,
+    setName,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+}
+
+export default Provider;
+
+Provider.prototypes = {
+  children: propTypes.any,
+}.isRequired;
