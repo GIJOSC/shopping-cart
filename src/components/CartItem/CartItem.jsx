bf720f5006fdf92bf976b1757bc1fb17deacf845
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-17 07:09:19 -0300

    feat/ cart item content and add icon

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+import { LiaCartArrowDownSolid } from "react-icons/lia";
+
 function CartItem() {
   return (
     <section>
       <img src="" alt="Imagem do produto" className="cart-item-image" />
+      <div className="cart-item-content">
+        <h3 className="cart-item-title">Titulo do Produto</h3>
+        <h3 className="cart-item-price">R$ 500,00</h3>
+        <button type="button" className="button__remove-item">
+          <LiaCartArrowDownSolid />
+        </button>
+      </div>
     </section>
   );
 }
