7867ed78d6fee5dfa062e9dc48074bd6ebd4a87d
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-17 06:28:02 -0300

    feat/add className and import css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Cart.css";
 
 function Cart() {
   return (
     <section>
-      <div>items do carrinho</div>
-      <div>resumo do carrinho</div>
+      <div className="cart-items">items do carrinho</div>
+      <div className="cart-resume">resumo do carrinho</div>
     </section>
   );
 }
