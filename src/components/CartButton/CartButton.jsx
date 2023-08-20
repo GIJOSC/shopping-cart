b56643f7a4cdff4b690cce9bb5b34461635fbf54
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-19 22:09:20 -0300

    feat/added quantity of products

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import "./CartButton.css";
+import AppContext from "../../context/AppContext";
 
 function CartButton() {
+  const { cartItems } = useContext(AppContext);
+
   return (
     <button type="button" className="cart__button">
       <HiOutlineShoppingCart />
-      <span className="cart-status">1</span>
+      <span className="cart-status">{cartItems.length}</span>
     </button>
   );
 }
